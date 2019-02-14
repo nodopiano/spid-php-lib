5a1e8c135a12ea3ac5db5ce2ca398e785eba7e72
Marco Bonomo <marco.bonomo@gmail.com>
2019-02-14 17:07:26 +0100

    workaround to work with saml2p response of poste.it

--- src/Spid/Saml/In/BaseResponse.php
+++ src/Spid/Saml/In/BaseResponse.php
@@ -20,6 +20,7 @@ class BaseResponse
 {
     private $response;
     private $xml;
+    private $prefix = 'saml';
 
     public function __construct(Saml $saml = null)
     {
@@ -31,13 +32,25 @@ class BaseResponse
         $xmlString = isset($_GET['SAMLResponse']) ?
             gzinflate(base64_decode($_GET['SAMLResponse'])) :
             base64_decode($_POST['SAMLResponse']);
-        
+
         $this->xml = new \DOMDocument();
         $this->xml->loadXML($xmlString);
 
         $root = $this->xml->documentElement->tagName;
-        
+
         switch ($root) {
+            case 'saml2p:Response':
+                // When reloading the acs page, POST data is sent again even if login is completed
+                // If login session already exists exit without checking the response again
+                if (isset($_SESSION['spidSession'])) {
+                    return;
+                }
+                if (is_null($saml)) {
+                    return;
+                }
+                $this->prefix = 'saml2';
+                $this->response = new Response($saml);
+                break;
             case 'samlp:Response':
                 // When reloading the acs page, POST data is sent again even if login is completed
                 // If login session already exists exit without checking the response again
@@ -48,6 +61,10 @@ class BaseResponse
                     return;
                 }
                 $this->response = new Response($saml);
+                break;
+            case 'saml2p:LogoutResponse':
+                $this->prefix = 'saml2';
+                $this->response = new LogoutResponse();
                 break;
             case 'samlp:LogoutResponse':
                 $this->response = new LogoutResponse();
@@ -69,19 +86,19 @@ class BaseResponse
         if (is_null($this->response)) {
             return true;
         }
-        
+
         $hasAssertion = $this->xml->getElementsByTagName('Assertion')->length > 0;
 
         $signatures = $this->xml->getElementsByTagName('Signature');
         if ($hasAssertion && $signatures->length == 0) {
-            throw new \Exception("Invalid Response. Response must contain at least one signature");
+            throw new \Exception('Invalid Response. Response must contain at least one signature');
         }
 
         $responseSignature = null;
         $assertionSignature = null;
         if ($signatures->length > 0) {
             foreach ($signatures as $key => $item) {
-                if ($item->parentNode->nodeName == 'saml:Assertion') {
+                if ($item->parentNode->nodeName == $this->prefix . ':Assertion') {
                     $assertionSignature = $item;
                 }
                 if ($item->parentNode->nodeName == $this->xml->firstChild->nodeName) {
@@ -89,11 +106,11 @@ class BaseResponse
                 }
             }
             if ($hasAssertion && is_null($assertionSignature)) {
-                throw new \Exception("Invalid Response. Assertion must be signed");
+                throw new \Exception('Invalid Response. Assertion must be signed');
             }
         }
         if (!SignatureUtils::validateXmlSignature($responseSignature, $cert) || !SignatureUtils::validateXmlSignature($assertionSignature, $cert)) {
-            throw new \Exception("Invalid Response. Signature validation failed");
+            throw new \Exception('Invalid Response. Signature validation failed');
         }
         return $this->response->validate($this->xml, $hasAssertion);
     }
